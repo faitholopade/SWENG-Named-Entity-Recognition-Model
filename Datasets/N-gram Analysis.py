03c7a055ad71d9998e959cd9127a124f7eee8a50
egaynor <egaynor@tcd.ie>
2023-03-04 02:07:08 +0000

    Cleaned up code

--- Datasets/N-gram Analysis.py
+++ Datasets/N-gram Analysis.py
@@ -1,18 +1,13 @@
 
 import pandas as pd
+from textblob import TextBlob
 
-
-#data = pd.Series([1,2,3])
-#print(data)
 column = [1, 1]
 head = 0
 attempt1 = str(pd.read_excel('Datasets\Dell Output PROCESSED.xlsx', usecols = column, header = head))
 print("attempt equals" + attempt1)
 
-from textblob import TextBlob
 blob = TextBlob(attempt1)
 unigrams = blob.ngrams(n=1)
 bigrams = blob.ngrams(n=2)
 trigrams = blob.ngrams(n=3)
-
-print(trigrams)
