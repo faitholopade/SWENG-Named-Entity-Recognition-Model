cd1e6cd8c7d2d5b9a024b3752c9c8ccd97b1f6a7
egaynor <egaynor@tcd.ie>
2023-03-04 01:52:42 +0000

    Trigrams formed!!!! :D

--- Datasets/N-gram Analysis.py
+++ Datasets/N-gram Analysis.py
@@ -5,9 +5,14 @@ import pandas as pd
 #data = pd.Series([1,2,3])
 #print(data)
 column = [1, 1]
-head = 2501
-attempt1 = pd.read_excel('Datasets\Dell Output PROCESSED.xlsx', usecols = column, header = head)
-print(attempt1)
+head = 0
+attempt1 = str(pd.read_excel('Datasets\Dell Output PROCESSED.xlsx', usecols = column, header = head))
+print("attempt equals" + attempt1)
 
 from textblob import TextBlob
 blob = TextBlob(attempt1)
+unigrams = blob.ngrams(n=1)
+bigrams = blob.ngrams(n=2)
+trigrams = blob.ngrams(n=3)
+
+print(trigrams)
