8a5f79f30efea421fff4b8ce7d4d86c6d8febfd1
donaghru <98951007+donaghru@users.noreply.github.com>
2023-04-13 15:37:02 +0100

    Update app.py

--- Software Implementations/Flask-API/app.py
+++ Software Implementations/Flask-API/app.py
@@ -40,8 +40,8 @@ def process():
             num_of_results = len(results)
 		
 	
-	return render_template("index.html",results=results,num_of_results = num_of_results)
+    return render_template("index.html",results=results,num_of_results = num_of_results)
 
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
